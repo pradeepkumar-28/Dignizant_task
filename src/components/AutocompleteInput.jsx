35f78b27d9b9cabee4899517fa6cce20972c1595
Pc-1997 <Pradeep.c@lahagora.in>
2024-01-06 15:58:18 +0530

    fix blur fun. in autoselet

--- src/components/AutocompleteInput.jsx
+++ src/components/AutocompleteInput.jsx
@@ -11,23 +11,26 @@ export default function ComboBox({ data, generateRatingIcons }) {
     <Autocomplete
       id="combo-box-demo"
       options={data}
-      freeSolo
+      clearOnBlur={false}
       sx={{ width: 450 }}
       getOptionLabel={(option) => option.label}
       open={open}
       onOpen={() => setOpen(true)}
       onChange={() => setOpen(false)}
-      renderOption={(props, option) => (
-        <Box component="li" {...props} className="options_box">
-          <Box className="option_header">
-            <h2 className="title">{option.label}</h2>
-            <h3 className="category">{option.category}</h3>
+      renderOption={(props, option) => {
+        console.log("option", option);
+        return (
+          <Box component="li" {...props} className="options_box">
+            <Box className="option_header">
+              <h2 className="title">{option.label}</h2>
+              <h3 className="category">{option.category}</h3>
+            </Box>
+            <Box className="Rating_box">
+              <h4>{generateRatingIcons(option.rating)}</h4>
+            </Box>
           </Box>
-          <Box className="Rating_box">
-            <h4>{generateRatingIcons(option.rating)}</h4>
-          </Box>
-        </Box>
-      )}
+        );
+      }}
       renderInput={(params) => (
         <TextField {...params} label="Enter Movie Name" />
       )}
